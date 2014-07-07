65891f4373e56824b2ccb024a80b5fb0c44dc777
jasku <jasku@localhost.localdomain>
2014-07-07 20:57:45 +0200

    Removed debug messages, changed to list

--- src/sk/tomsik68/bukkit/autocommand/args/ArgumentParsers.java
+++ src/sk/tomsik68/bukkit/autocommand/args/ArgumentParsers.java
@@ -1,5 +1,6 @@
 package sk.tomsik68.bukkit.autocommand.args;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.apache.commons.lang.Validate;
@@ -18,20 +19,25 @@ public class ArgumentParsers {
     }
 
     public static Object[] parse(Class[] classes, String[] args) throws ArgumentParserException {
+
+        // 1) split up args also based on quotes
         args = convertArgs(args);
+        // 2) each class needs to be parsed from args list
+        for (String arg : args) {
+
+        }
         return null;
     }
 
     static String[] convertArgs(String[] args) {
+        ArrayList<String> resultList = new ArrayList<String>();
         // join arguments into 1 string
         StringBuilder sb = new StringBuilder();
         for (String arg : args) {
             sb = sb.append(arg).append(' ');
         }
-        String[] newArgs = new String[0];
-        // split up arguments once again using advanced strategies
+        // split up arguments once again respecting quotes
         String argsString = sb.toString();
-        System.out.println("ArgsString: " + argsString);
         int index = 0;
         String tempArg = "";
         boolean inQuotes = false;
@@ -39,37 +45,23 @@ public class ArgumentParsers {
             char c = argsString.charAt(index);
 
             if (c == '"') {
-                System.out.println("-Quote");
-
-                System.out.println("--New arg");
                 if (tempArg.length() > 1) {
-                    System.out.println("---Add the previous arg");
-                    String[] newArgs2 = new String[newArgs.length + 1];
-                    System.arraycopy(newArgs, 0, newArgs2, 0, newArgs.length);
-                    newArgs2[newArgs.length] = tempArg;
-                    newArgs = newArgs2;
+                    resultList.add(tempArg);
                 }
-                System.out.println("--tempArg is now empty");
                 tempArg = "";
                 inQuotes = !inQuotes;
             } else if (c == ' ' && !inQuotes) {
-                System.out.println("-whitespace!");
                 if (tempArg.length() > 1) {
-                    System.out.println("--Add previous arg");
-                    String[] newArgs2 = new String[newArgs.length + 1];
-                    System.arraycopy(newArgs, 0, newArgs2, 0, newArgs.length);
-                    newArgs2[newArgs.length] = tempArg;
-                    newArgs = newArgs2;
+                    resultList.add(tempArg);
                 }
                 tempArg = "";
             } else {
                 tempArg += c;
-                System.out.print(c);
             }
 
             ++index;
         }
-        return newArgs;
+        return resultList.toArray(new String[0]);
     }
 
 }

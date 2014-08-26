86832676a75b55e704c2fc8ca5d03af9f377d48a
tomsik68 <tomsik68@gmail.com>
2014-08-26 14:30:20 +0200

    Added possibility to escape quotes in args by \\

--- src/sk/tomsik68/bukkit/autocommand/args/ArgumentParsers.java
+++ src/sk/tomsik68/bukkit/autocommand/args/ArgumentParsers.java
@@ -69,6 +69,9 @@ public class ArgumentParsers {
                 tempArg = "";
             } else {
                 tempArg += c;
+                if(c == '\\'){
+                    tempArg += argsString.charAt(++index);
+                }
             }
 
             ++index;
--- src/sk/tomsik68/bukkit/autocommand/args/TestArgumentSplitter.java
+++ src/sk/tomsik68/bukkit/autocommand/args/TestArgumentSplitter.java
@@ -8,10 +8,10 @@ public class TestArgumentSplitter {
 
     @Test
     public void test() {
-        String command = "stopat \"world of derp\" \"something number one\" something2  ";
+        String command = "stopat \"world\\\" of derp\" \"something number one\" something2  ";
         String[] badArgs = command.split(" ");
         String[] goodArgs = new String[] {
-                "stopat", "world of derp", "something number one", "something2"
+                "stopat", "world\\\" of derp", "something number one", "something2"
         };
         String[] converted = ArgumentParsers.getArgumentsFromArray(badArgs);
         System.out.println("==================================");
